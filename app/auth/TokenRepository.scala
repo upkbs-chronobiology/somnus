a8e73bfefc1ad8d0adcedbd1ad2862201829cd67
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2019-11-11 14:27:49 +0100

    Use simple LRU map cache for session auth repo

--- app/auth/TokenRepository.scala
+++ app/auth/TokenRepository.scala
@@ -1,6 +1,7 @@
 package auth
 
 import java.time.Instant
+import java.util.Collections
 import java.util.concurrent.TimeUnit
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,6 +14,7 @@ import com.mohiva.play.silhouette.impl.authenticators.BearerTokenAuthenticator
 import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
 import javax.inject.Inject
 import javax.inject.Singleton
+import org.apache.commons.collections4.map.LRUMap
 import org.joda.time.DateTime
 import play.api.db.slick.DatabaseConfigProvider
 import slick.jdbc.H2Profile.api._
@@ -54,11 +56,19 @@ class UserSessionTable(tag: Tag) extends Table[UserSession](tag, "user_session")
 class TokenRepository @Inject()(dbConfigProvider: DatabaseConfigProvider, credentialsProvider: CredentialsProvider)
   extends AuthenticatorRepository[BearerTokenAuthenticator] {
 
+  private val MaxCachedSessions = 1000
+  private val authCache = Collections.synchronizedMap(new LRUMap[String, BearerTokenAuthenticator](MaxCachedSessions))
+
   def sessions = TableQuery[UserSessionTable]
 
   def dbConfig = dbConfigProvider.get[JdbcProfile]
 
+  // ignore return wart as cache lookup is an intended side effect
+  @SuppressWarnings(Array("org.wartremover.warts.Return"))
   override def find(id: String): Future[Option[BearerTokenAuthenticator]] = {
+    val cached = Option(authCache.get(id))
+    if (cached.isDefined) return Future.successful(cached) // scalastyle:ignore return
+
     val sessionById = sessions.filter(_.id === id).result.headOption
     dbConfig.db.run(sessionById).map(_.map(UserSession.toBearerTokenAuthenticator(_, credentialsProvider)))
   }
@@ -69,7 +79,9 @@ class TokenRepository @Inject()(dbConfigProvider: DatabaseConfigProvider, creden
       case Some(_) => throw new IllegalArgumentException(s"BearerTokenAuthenticator with id ${authenticator.id} already exists")
       case None =>
         dbConfig.db.run(sessions += UserSession.fromBearerTokenAuthenticator(authenticator)) map {
-          case 1 => authenticator
+          case 1 =>
+            authCache.put(authenticator.id, authenticator)
+            authenticator
           case 0 => throw new IllegalStateException("Insertion of authenticator to database failed")
           case _ => throw new IllegalStateException("Number of authenticator insertions was neither 0 nor 1")
         }
@@ -77,11 +89,13 @@ class TokenRepository @Inject()(dbConfigProvider: DatabaseConfigProvider, creden
   }
 
   override def update(authenticator: BearerTokenAuthenticator): Future[BearerTokenAuthenticator] = {
+    authCache.put(authenticator.id, authenticator)
     dbConfig.db.run(sessions.filter(_.id === authenticator.id)
       .update(UserSession.fromBearerTokenAuthenticator(authenticator))).map(_ => authenticator)
   }
 
   override def remove(id: String): Future[Unit] = {
+    authCache.remove(id)
     dbConfig.db.run(sessions.filter(_.id === id).delete).map(_ => Unit)
   }
 }
