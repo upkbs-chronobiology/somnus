07ca8e42a1a33267ce0bf967a19ff7f71d11b4d3
Cedric Reichenbach <cedric.reichenbach@gmail.com>
2019-11-18 15:58:14 +0100

    Add generic 'poke' endpoint; expose 'Date' header through CORS
    This is related to somnus-app issue #75 (problems when time out of sync):
    https://github.com/upkbs-chronobiology/somnus-app/issues/75

---
+++ app/v1/GeneralController.scala
@@ -0,0 +1,10 @@
+package v1
+
+import javax.inject.Inject
+
+class GeneralController @Inject()(rcc: RestControllerComponents) extends RestBaseController(rcc) {
+  /**
+    * Simple empty-body response, useful for e.g. time or connectivity checks.
+    */
+  def poke = Action(Ok(""))
+}
